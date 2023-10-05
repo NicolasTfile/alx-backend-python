cd0c67d3dc7544c6967a567e9c698eadb3dec570
Agunloye Olaniyi <nicolastfile@gmail.com>
2023-10-05 16:41:03 +0100

    Worked on another type annotation

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -18,4 +18,4 @@ def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     Returns:
         Tuple: A tuple with the string key and the square of v
     """
-    return (k, float(v ** 2))
+    return k, float(v ** 2)
