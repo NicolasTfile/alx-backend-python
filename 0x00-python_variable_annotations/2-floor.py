5aca66887ee4dab2f066f0e114942f52c4006d48
Agunloye Olaniyi <nicolastfile@gmail.com>
2023-10-05 15:58:49 +0100

    Worked on another type annotation

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import math
+
+"""
+This script defines a function to calculate the floor of a float.
+"""
+
+
+def floor(n: float) -> int:
+    """
+    Calculate and return the floor of a float
+
+    Args:
+        n (float): The input floating-point number
+
+    Returns:
+        int: The floor of the input number as an integer
+    """
+    return math.floor(n)
