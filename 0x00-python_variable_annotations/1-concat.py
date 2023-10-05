a19d369fcd815ab4bb20f51c6265f343ec81a4bf
Agunloye Olaniyi <nicolastfile@gmail.com>
2023-10-05 15:51:30 +0100

    Worked on another type annotation

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+a type-annotated function named concat
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenate two strings and return the result
+
+    Args:
+        str1: The first string
+        str2: The second string
+    """
+    return str1 + str2
