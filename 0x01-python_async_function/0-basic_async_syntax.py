ff8c1baf8b34a48bc1aca120ec29bd01e00579b7
Agunloye Olaniyi <nicolastfile@gmail.com>
2023-10-09 12:04:34 +0100

    Created and fixed an asynchronous coroutine file

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -6,6 +6,7 @@ Waits for a random delay between 0 and max_delay seconds (inclusive)
 import asyncio
 import random
 
+
 async def wait_random(max_delay: int = 10) -> float:
     """
     Wait for a random delay between 0 and max_delay seconds and return it
